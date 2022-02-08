b6a35cf2e586f29d0ef8f1e9b778e7b0f8e135a0
saeed <saeed.alijani@aut.ac.ir>
2022-02-08 17:49:42 +0330

    update OrganizeFiles() to get directory in obj call

--- src/clean.py
+++ src/clean.py
@@ -10,21 +10,21 @@ from src.utils.io import read_json
 
 class OrganizeFiles:
     """this class is used to clean a directories based on extensions"""
-    def __init__(self,directory):
-        self.direcory = Path(directory)
-        if not self.direcory.exists():
-            raise FileNotFoundError("The directory does not exist")
-
+    def __init__(self):
         ext_dirs = read_json(DATA_DIR / 'extensions.json')
         self.extensions_dest = {}
         for dir_name, ext_list in ext_dirs.items():
             for ext in ext_list :
                 self.extensions_dest[ext] = dir_name
 
-    def __call__(self):
+    def __call__(self, directory):
         """Organize files in a directory by moving them to their respective directories"""
-        logger.info("Organizing the files in the {self.directory}...")
-        for file_path in self.direcory.iterdir():
+
+        direcory = Path(directory)
+        if not direcory.exists():
+            raise FileNotFoundError("The directory does not exist")
+        logger.info("Organizing the files in the {directory}...")
+        for file_path in direcory.iterdir():
             #ignore directiries
             if file_path.is_dir():
                 continue
@@ -33,10 +33,10 @@ class OrganizeFiles:
                 continue
 
             if file_path.suffix not in self.extensions_dest :
-                DEST_DIR = self.direcory / 'others'
+                DEST_DIR = direcory / 'others'
             else :
                 #makes destionation directories
-                DEST_DIR = self.direcory / self.extensions_dest[file_path.suffix]
+                DEST_DIR = direcory / self.extensions_dest[file_path.suffix]
                 DEST_DIR.mkdir(exist_ok=True)
                 logger.info(f"Moving {file_path} to {DEST_DIR}")
                 shutil.move(str(file_path), str(DEST_DIR))
@@ -44,7 +44,7 @@ class OrganizeFiles:
                 pass
 
 if __name__ == "__main__":
-    org_files = OrganizeFiles(DATA_DIR / '/home/saeed/Desktop/tmporary')
-    org_files()
+    org_files = OrganizeFiles()
+    org_files('/home/saeed/Desktop/tmporary')
     logger.info("Done!")
 
